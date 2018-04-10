4270bee8b35587e3d445303ae1fd830564cf27c4
jcalisto <joao.s.calisto@tecnico.ulisboa.pt>
2018-04-10 19:18:38 +0100

    Begin

---
+++ app/src/main/java/com/cmu/group22/hoponcmu/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.cmu.group22.hoponcmu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
