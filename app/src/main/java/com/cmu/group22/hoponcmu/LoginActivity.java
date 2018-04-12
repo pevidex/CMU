3497ab820fb02d6ebc12cc740019d512b6ea1c71
atlas <154213158@qq.com>
2018-04-12 15:18:48 +0100

    main activity of the app

--- app/src/main/java/com/cmu/group22/hoponcmu/LoginActivity.java
+++ app/src/main/java/com/cmu/group22/hoponcmu/LoginActivity.java
@@ -1,15 +1,33 @@
 package com.cmu.group22.hoponcmu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toolbar;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+/*
+        btLogin = (Button) findViewById(R.id.btLogin);
 
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText userName = (EditText) findViewById(R.id.etUsername);
+                EditText code = (EditText) findViewById(R.id.etCode);
+                //new LoginTask(LoginActivity.this).execute(userName.getText().toString(),code.getText().toString());
+            }
+        });
+        Intent myIntent = new Intent(this, MainActivity.class);
+        startActivity(myIntent);
+*/
     }
 }
